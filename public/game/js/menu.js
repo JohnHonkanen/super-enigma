3a187d0330fd37ead0c3e06917ba839a233cd247
Lapalm <anthony_palm@hotmail.co.jp>
2019-04-20 05:34:22 +0100

    Added audio and loop to game

--- public/game/js/menu.js
+++ public/game/js/menu.js
@@ -4,9 +4,22 @@ class Menu extends Phaser.Scene
     {
         super({key:"Menu"});
     }
-    
+
+    preload()
+    {
+        this.load.audio('music', ['assets/audio/background-looping.ogg', 'assets/audio/background-looping.mp3']);
+    }
+
     create()
     {
+
         var startText = new MenuItem(this, 'Start Game', 100, 100, 'Level1');
+        const music = this.sound.add('music');
+
+        // //	play: function (marker, position, volume, loop, forceRestart)
+        music.play('', 0, 1, true);
+
+
+
     }
 }
